55dc74aa70595f5fb8cb1e7092320271f3f994e8
shshwdr <cs.vivian04@gmail.com>
2018-08-20 02:16:21 -0700

    add splash, fix some UI

---
+++ Assets/aspectRatioForLayoutElement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class aspectRatioForLayoutElement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float ph = 800;
+        float pw = 500;
+        float h = transform.parent.GetComponent<RectTransform>().rect.height;
+        float w = pw * h / ph;
+        GetComponent<LayoutElement>().preferredWidth = w;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(w, h);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
