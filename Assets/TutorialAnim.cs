b27a4df043cd6365762d580d335c51e187bd6e9f
shshwdr <cs.vivian04@gmail.com>
2018-08-22 20:40:32 -0700

    try to add tutorial anim

---
+++ Assets/TutorialAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialAnim : MonoBehaviour {
+    public GameObject circleObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayerMoveToCircle()
+    {
+        (TutorialPlayer.Instance).MoveToTarget(circleObject);
+    }
+    public void ResetAnim()
+    {
+        //TutorialPlayer.Instance.resetAnim
+    }
+}
---
+++ Assets/TutorialPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPlayer : Player {
+    // Use this for initialization
+    //infinite hp
+    
+    public void ResetAnim()
+    {
+
+    }
+
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
